44317335817ee36bb8778be0b268136f1aba8f56
Olamide Williams <104972300+circleofwilliams@users.noreply.github.com>
2023-04-20 00:24:39 +0100

    updating the FIFO algorithm

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,48 +1,29 @@
 #!/usr/bin/python3
-""" FIFO Caching """
-
-from base_caching import BaseCaching
+""" FIFOCache module
+    first in first out algorithm
+"""
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ FIFO caching """
-
-    def __init__(self):
-        """ Constructor """
-        super().__init__()
-        self.queue = []
-
+    """ A caching system
+    """
     def put(self, key, item):
-        """ Puts item in cache """
-        if key is None or item is None:
-            return
-
-        if key not in self.queue:
-            self.queue.append(key)
-        else:
-            self.mv_last_list(key)
-
-        self.cache_data[key] = item
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first = self.get_first_list(self.queue)
-            if first:
-                self.queue.pop(0)
-                del self.cache_data[first]
-                print("DISCARD: {}".format(first))
+        """ Add an item in the cache and check if the cache is full or not.
+        """
+        if key and item is not None:
+            if (key not in self.cache_data.keys()) and \
+                    (len(self.cache_data) >= BaseCaching.MAX_ITEMS):
+                print(f'DISCARD: {next(iter(self.cache_data))}')
+                first_key = next(iter(self.cache_data))
+                self.cache_data.pop(first_key)
+            self.cache_data[key] = item
+        return
 
     def get(self, key):
-        """ Gets item from cache """
-        return self.cache_data.get(key, None)
-
-    def mv_last_list(self, item):
-        """ Moves element to last idx of list """
-        length = len(self.queue)
-        if self.queue[length - 1] != item:
-            self.queue.remove(item)
-            self.queue.append(item)
-
-    @staticmethod
-    def get_first_list(array):
-        """ Get first element of list or None """
-        return array[0] if array else None
+        """ Get an item by key
+        """
+        try:
+            return self.cache_data[key]
+        except Exception:
+            return None 
